cff788106cf0935bcf5737b8e3e557e7a7900573
maryn <maryn@telepathics.xyz>
2018-04-10 11:39:01 -0600

    Server hypothetical code

--- server.js
+++ server.js
@@ -7,15 +7,6 @@ const WebSocket = require('ws');
 const app = express();
 app.use(express.static('public'));
 
-function sendAll(message) {
-	for (var i=0; i < CLIENTS.length; i++) {
-		// check readystate or else it'll crash
-		if (CLIENTS[i].readyState == CLIENTS[0].OPEN) {
-			CLIENTS[i].send(message);
-		}
-	}
-}
-
 var server = app.listen(process.env.PORT || 5000, function() {
 	console.log("Server is ready and listening!");
 });
@@ -25,22 +16,57 @@ var server = app.listen(process.env.PORT || 5000, function() {
 // ===========================
 
 var wss = new WebSocket.Server({ server: server });
-var CLIENTS = [];
+var USERS = [];
+
+function sendAll(message) {
+	for (var i=0; i < USERS.length; i++) {
+		// check readystate or else it'll crash
+		if (USERS[i].readyState == USERS[0].OPEN) {
+			USERS[i].send(message);
+		}
+	}
+}
+
+function findPair(user) {
+	USERS.forEach(function (pair) {
+		if (pair.pair == null && pair.username != user.username) {
+			pair.pair = user.username;
+			user.username = pair.username;
+			console.log("Paired" + user.username + " with " + pair.username);
+
+			return;
+		}
+
+		// TODO if no pair found...
+		console.log("No empty pairs.  Please try again letter.");
+	})
+}
 
 wss.on('connection', function (wsclient) {
-	CLIENTS.push(ws);
 
 	wsclient.on('message', function (data) {
-		console.log("Client ", wsclient, " said ", data);
+		var m = JSON.parse(data);
+		if (m.action == 'ident') {
+			var client = m.username;
+			USERS[client] = {
+				username: username,
+				ws: wsclient,
+				pair: null
+			};
+		} else if (m.action == 'pair') {
+			var user = players[m.username];
+			findPair(user);
+		}
+
+		console.log("User ", wsclient, " said ", data);
 	});
 
 	ws.on('close', function(client) {
-		// remove one socket from array of clients
-		CLIENTS.splice(CLIENTS.indexOf(client), 1);
+		USERS.splice(USERS.indexOf(client), 1); // remove one socket from array of clients
 	});
 
 	ws.on('error', function(client) {
-		// remove one socket from array if error
-		CLIENTS.splice(CLIENTS.indexOf(client), 1);
+		USERS.splice(USERS.indexOf(client), 1); // remove one socket from array if error
 	});
+
 });
