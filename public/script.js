cc824d4c88de1e678a596bb7e6121ed046944fa7
maryn <maryn@telepathics.xyz>
2018-04-10 10:59:48 -0600

    Generate username from randomuser API

--- public/script.js
+++ public/script.js
@@ -1,13 +1,23 @@
 let apiURL = 'https://localhost:5000'
 
 // Generate Username & Color
-const dicAdj = ["adamant", "adroit", "amatory", "animistic", "antic", "arcadian", "arctic", "baleful", "bellicose", "bilious", "boorish", "calamitous", "caustic", "cerulean", "comely", "concomitant", "contumacious", "corpulent", "crapulous", "defamatory", "didactic", "silly", "dowdy", "efficacious", "effulgent", "egregious", "endemic", "equanimous", "execrable", "fastidious", "feckless", "fecund", "friable", "fulsome", "garrulous", "guileless", "gustatory", "heuristic", "histrionic", "hubristic", "incendiary", "insidious", "insolent", "creepy", "inveterate", "spooky", "irksome", "jocular", "judicious", "lachrymose", "limpid", "loquacious"];
-const dicNoun = ["ninja", "chair", "pancake", "statue", "unicorn", "rainbow", "laser", "senor", "bunny", "captain", "nibblet", "cupcake", "carrot", "gnome", "glitter", "potato", "salad", "toejam", "curtain", "beet", "toilet", "exorcism", "egg", "mermaid", "lunch", "dragon", "jellybean", "snake", "doll", "bush", "cookie", "apple", "ice cream", "ukulele", "kazoo", "banjo", "vaccuum", "circus", "trampoline", "carousel", "carnival", "locomotive", "balloon", "mantis", "animator", "artisan", "artist", "colorist", "inker", "coppersmith", "director", "designer", "flatter", "stylist", "leadman", "limner"];
-const dicColor = ["#90ee90", "#ffc0cb", "#ffa500", "#add8e6", "#d9f7b4", "#e1c0eb", "#64ebd1"];
+const colors = ["#90ee90", "#ffc0cb", "#ffa500", "#add8e6", "#d9f7b4", "#e1c0eb", "#64ebd1"];
 
 function randomEl(list) {
 	var i = Math.floor(Math.random() * list.length);
 	return list[i];
+}
+
+function generateUser() {
+	return fetch("https://randomuser.me/api", {
+		method: 'get',
+		dataType: 'json'
+	}).then(function (res) {
+		return res.json();
+	}).then(function (data) {
+		landing.userName = data['results'][0]['login']['username'].replace(/[0-9]/g, '');
+		landing.userColor = colors[Math.floor(Math.random() * colors.length)];
+	});
 }
 
 function checkForMatch() {
@@ -26,19 +36,21 @@ let landing = new Vue({
 	el: '#landing-page',
 	data: {
 		visible: true,
-		userName: `${randomEl(dicAdj)} ${randomEl(dicNoun)}`,
-		userColor: dicColor[Math.floor(Math.random() * dicColor.length)]
+		userName: '',
+		userColor: colors[Math.floor(Math.random() * colors.length)]
 	},
 	methods: {
 		generate: function() {
-			this.userName = `${randomEl(dicAdj)} ${randomEl(dicNoun)}`;
-			this.userColor = dicColor[Math.floor(Math.random() * dicColor.length)];
+			generateUser();
 		},
 		search: function() {
 			this.visible = false;
 			loading.visible = true;
 			checkForMatch();
 		}
+	},
+	created: function() {
+		generateUser();
 	}
 });
 
