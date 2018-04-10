25bb8b29c35bec8e4fe64febcbfc9e343e25335d
maryn <maryn@telepathics.xyz>
2018-04-10 11:16:25 -0600

    Restructure data

--- server.js
+++ server.js
@@ -4,45 +4,34 @@ const session = require('express-session');
 const express = require('express');
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({
-	port: 8080,
-	perMessageDeflate: {
-		zlibDeflateOptions: { // See zlib defaults.
-			chunkSize: 1024,
-			memLevel: 7,
-			level: 3,
-		},
-		zlibInflateOptions: {
-			chunkSize: 10 * 1024
-		},
-		// Other options settable:
-		clientNoContextTakeover: true,	// Defaults to negotiated value.
-		serverNoContextTakeover: true,	// Defaults to negotiated value.
-		clientMaxWindowBits: 10,		// Defaults to negotiated value.
-		serverMaxWindowBits: 10,		// Defaults to negotiated value.
-		// Below options specified as default values.
-		concurrencyLimit: 10,			// Limits zlib concurrency for perf.
-		threshold: 1024,				// Size (in bytes) below which messages
-		// should not be compressed.
-	}
-});
-
-var CLIENTS = [];
 const app = express();
 app.use(express.static('public'));
 
-// ws.on('open', function open() {
-// 	ws.send('something');
-// });
+function sendAll(message) {
+	for (var i=0; i < CLIENTS.length; i++) {
+		// check readystate or else it'll crash
+		if (CLIENTS[i].readyState == CLIENTS[0].OPEN) {
+			CLIENTS[i].send(message);
+		}
+	}
+}
 
-// ws.on('message', function incoming(data) {
-// 	console.log(data);
-// });
+var server = app.listen(process.env.PORT || 5000, function() {
+	console.log("Server is ready and listening!");
+});
 
-wss.on('connection', function(ws) {
+// ===========================
+// WEBSOCKET SERVER CODE BELOW
+// ===========================
+
+var wss = new WebSocket.Server({ server: server });
+var CLIENTS = [];
+
+wss.on('connection', function (wsclient) {
 	CLIENTS.push(ws);
-	ws.on('message', function(message) {
 
+	wsclient.on('message', function (data) {
+		console.log("Client ", wsclient, " said ", data);
 	});
 
 	ws.on('close', function(client) {
@@ -54,17 +43,4 @@ wss.on('connection', function(ws) {
 		// remove one socket from array if error
 		CLIENTS.splice(CLIENTS.indexOf(client), 1);
 	});
-});
-
-function sendAll(message) {
-	for (var i=0; i < CLIENTS.length; i++) {
-		// check readystate or else it'll crash
-		if (CLIENTS[i].readyState == CLIENTS[0].OPEN) {
-			CLIENTS[i].send(message);
-		}
-	}
-}
-
-app.listen(process.env.PORT || 5000, function() {
-	console.log("Server is ready and listening!");
 });
